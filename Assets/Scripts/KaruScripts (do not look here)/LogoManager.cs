35f8fc933bd4a586d32fa4efb3f4bc556991c918
Rubus <66851315+Rubusderg@users.noreply.github.com>
2024-11-30 20:42:36 +0100

    GameFinished

---
+++ Assets/Scripts/KaruScripts (do not look here)/LogoManager.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LogoManager : MonoBehaviour
+{
+
+    [SerializeField] float delay;
+    [SerializeField] int introScene;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(WaitDelay());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    IEnumerator WaitDelay()
+    {
+        yield return new WaitForSeconds(delay);
+        SceneManager.LoadScene(introScene);
+    }
+
+
+}
